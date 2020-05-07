f6b6bbf8c54e39b42dbd992da09827910d052101
Cem TASKIN <cemtaskin82@gmail.com>
2020-05-07 11:46:35 +0300

    İnitial Commit

---
+++ MessagingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using MessagingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessagingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
